cca8b644f394877d9b94b890152897e7e08eabdb
tudvh <dangtu9520@gmail.com>
2025-01-01 19:07:18 +0700

    first commit

---
+++ Bot.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+
+const axios = require('axios')
+
+class Bot {
+  constructor() {
+    this.baseUrl = process.env.BASE_URL
+    this.joinedChannelIds = []
+
+    this.api = axios.create({
+      baseURL: this.baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer`,
+      },
+    })
+    this.login()
+  }
+
+  async login() {
+    try {
+      const response = await this.api.post('/auth/bot-login', {
+        email: process.env.BOT_EMAIL,
+      })
+      const { accessToken, joinedChannelIds } = response.data
+      this.api.defaults.headers.Authorization = `Bearer ${accessToken}`
+      this.joinedChannelIds = joinedChannelIds
+      console.log('Bot logged in')
+    } catch (error) {
+      console.error('Error logging in:', error.message)
+      throw error
+    }
+  }
+
+  async sendMessage(roomId, content) {
+    try {
+      const response = await this.api.post('/messages', {
+        roomId,
+        content,
+      })
+      console.log('Send message success: ', content)
+      return response.data
+    } catch (error) {
+      console.error('Error sending message:', error.message)
+      throw error
+    }
+  }
+
+  async getMeetingChannel() {
+    try {
+      const response = await this.api.get(
+        'rooms/get-all-free-room?channelId=dcca15f2-8f89-4c15-a272-cdf16ae905bb',
+      )
+      const rooms = response.data
+      if (!rooms) {
+        throw new Error('Meeting room not found')
+      }
+      return rooms
+    } catch (error) {
+      console.error('Error getting meeting room:', error.message)
+      throw error
+    }
+  }
+}
+
+module.exports = new Bot()
---
+++ index.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const Pusher = require('pusher-js')
+const Bot = require('./Bot')
+
+const pusher = new Pusher('effbfb47a66d70473872', {
+  cluster: 'ap1',
+  encrypted: true,
+})
+
+const channelId = 'dcca15f2-8f89-4c15-a272-cdf16ae905bb'
+const roomId = 'c719e57b-0501-49c9-aee5-c9359c0530de'
+
+const channel = pusher.subscribe(roomId)
+
+channel.bind('new-message', async data => {
+  switch (data.content) {
+    case '*meeting help':
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve()
+        }, 2000)
+      })
+      Bot.sendMessage(
+        roomId,
+        `*meeting now: tạo một phòng họp ngay lập tức;
+        \n
+        *meeting schedule: lên lịch tạo một phòng họp`,
+      )
+      break
+    case '*meeting now':
+      const rooms = await Bot.getMeetingChannel()
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve()
+        }, 1500)
+      })
+      Bot.sendMessage(roomId, `/channels/${channelId}/${rooms[0]}`)
+      break
+    // case '*metting schedule':
+    //     //Lên lịch meeting
+    //     break;
+  }
+})
+
+pusher.connection.bind('connected', () => {
+  console.log('Đã kết nối tới Pusher')
+})
+
+pusher.connection.bind('disconnected', () => {
+  console.log('Đã ngắt kết nối khỏi Pusher')
+})
+
+pusher.connection.bind('error', err => {
+  console.error('Lỗi kết nối Pusher:', err)
+})
+
+function subscribeToMultipleChannels(channelNames) {
+  channelNames.forEach(channelName => {
+    const channel = pusher.subscribe(channelName)
+
+    channel.bind('your-event-name', data => {
+      console.log(`Sự kiện từ kênh ${channelName}:`, data)
+    })
+  })
+}
+
+function unsubscribeFromChannel(channelName) {
+  pusher.unsubscribe(channelName)
+  console.log(`Đã hủy đăng ký kênh ${channelName}`)
+}
+
+process.on('SIGINT', () => {
+  console.log('Đóng kết nối Pusher...')
+  pusher.disconnect()
+  process.exit()
+})
